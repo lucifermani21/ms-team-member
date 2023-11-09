0f5441ef9a55428b3995de016798c4591bb491c1
Manpreet Singh <manpreetsinghmanidev@gmail.com>
2023-11-09 21:42:03 +0530

    global varibale update

--- inc/plugin-settings.php
+++ inc/plugin-settings.php
@@ -1,9 +1,3 @@
-<?php 
-function MS_plugin_register_settings() {
-    echo "";
-}
-add_action( 'admin_init', 'MS_plugin_register_settings' );
-?>
 <div class="wrap">
     <h1>Team Member Plugin Settings</h1>
     <hr/>
